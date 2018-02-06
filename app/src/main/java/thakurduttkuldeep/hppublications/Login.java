a1a4479942b6d454db3b5fbb0ad765fd10addc32
Thakur Ji <thakurduttkuldeep123@gmail.com>
2018-02-06 18:53:36 +0530

    hjgjhgjdgjdjdghs

---
+++ app/src/main/java/thakurduttkuldeep/hppublications/Login.java
@@ -0,0 +1,18 @@
+package thakurduttkuldeep.hppublications;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public  void  fun()
+    {
+        System.out.print("TYhis is n0thkng");
+    }
+}
